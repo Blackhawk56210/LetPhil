52645e84ac7c545caa46ed9b28799e7c272e1d63
Colby <maquiret@gmail.com>
2025-02-17 17:18:26 -0600

    updating password generator

--- PassWordGenerator/script.js
+++ PassWordGenerator/script.js
@@ -1,26 +1,50 @@
 //character variables
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-//random index
+//index
 const passwordLength = 12;
 const randomIndex = Math.floor(Math.random() * characters.length);
 const randomCharacters = characters[randomIndex];
+const slider = document.getElementById("lengthSlider");
+const lengthDisplay = document.getElementById("lengthValue");
+const generateButton = document.getElementById("generate");
+const output = document.getElementById("output");
 
 //random password Generation
 function generateRandomPassword(passwordLength) {
     let password = "";
     for (let i = 0; i < passwordLength; i++){
-    password += randomCharacters;
-
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters[randomIndex];
   }
-  return password;
+  return password; 
 }
 
-//password Generator
+//password Generator button
 document.getElementById("generate").addEventListener("click", () => {
  const password = generateRandomPassword(12);
  document.getElementById("output").textContent = password;
 });
 
+//update length display when slider moves
+slider.addEventListener("input", () => {
+  LengthDisplay.textContent = slider.value;
+});
+
+generateButton.addEventListener("click", () => {
+  const passwordLength = slider.value;
+  const password = generateRandomPassword(passwordLength);
+  output.textContent = password;
+})
+
+
+//length-selector tickmarks
+// const slider = document.getElementById("slider");
+// const sliderValue = document.getElementById("sliderValue");
+
+// sliderValue.addEventListener("input", () => {
+//   sliderValue.textContent = slider.Value;
+// })
+
 //hope it works
-console.log(generateRandomPassword(characters));
+console.log(generateRandomPassword(12)); 
